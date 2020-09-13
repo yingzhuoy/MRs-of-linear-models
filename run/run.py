4954407ec18241d1c6f30f911af774f2f5ce2113
yingzhuoy <yingzhuoy@qq.com>
2020-09-13 10:53:12 +0800

    modified run

--- run/run.py
+++ run/run.py
@@ -22,14 +22,14 @@ if __name__ == '__main__':
 
 	#n_train, n_test, n_features, n_redundant, n_classes, neg_class(算法是svm时 neg_class = -1, 算法是logreg时， neg_class = 0)
 	
-	#datasets = CreateDataset(400,100,10,0,2,-1)
-	datasets = CreateDataset(400,100,10,0,2,0)
+	datasets = CreateDataset(400,100,10,0,2,-1)
+	#datasets = CreateDataset(400,100,10,0,2,0)
 	
-	#lr = SVM_CVX_L2_m12()
-	lr = LR_GA_m1()
+	lr = SVM_CVX_L1_gv()
+	#lr = LR_GA_m1()
 
-	#test = LinearMRs(lr.fit, datasets.classification, hyp_classification, 50)
-	test = LinearMRs(lr.fit, datasets.classification, sig_classification, 50)
+	test = LinearMRs(lr.fit, datasets.classification, hyp_classification, 50)
+	#test = LinearMRs(lr.fit, datasets.classification, sig_classification, 50)
 
 	test.MR1()
 	test.MR2()
