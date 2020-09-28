a13ac1912a7e24badeb4e4d0c4bdc848a6cad35f
lizenan <lizenan1995@foxmail.com>
2020-09-28 10:47:46 +0800

    GD_gv for Logistic_regression

--- algorithms/Logistic_regression/GD/GD_gv.py
+++ algorithms/Logistic_regression/GD/GD_gv.py
@@ -4,19 +4,38 @@ import sys
 sys.path.append(r'..')
 
 # gradAscent
+def backtracking(l0, w0, X, y):
+    # update x
+    epsilon = 1e-10
+    beta = 0.5
+    l = l0
+    h0 = sigmoid(X * w0)
+    L0 = y.T*np.log(h0+epsilon) + (1-y).T * np.log(1+epsilon-h0)
+    error = y - h0  # vector subtraction\
+    g0 = - X.T * error   
+    for k in range(128):
+        wp = w0 - l * g0
+        h = sigmoid(X * wp)
+        Lw = y.T * np.log(h+epsilon) + (1-y).T * np.log(1+epsilon-h)
+        gt = (w0-wp) / l
+        if Lw > L0 - l *(g0.T*gt) + 0.5*l*gt.T*(gt):
+            l = beta * l
+        else:
+            break
+            
+    return wp, l
 
+def sigmoid(x):
+    # avoid overflow
+    return .5 * (1 + np.tanh(.5 * x))
+    # return 1/(1+np.exp(-x))
 
 class GD_gv():
 
-    def sigmoid(self, x):
-        # avoid overflow
-        return .5 * (1 + np.tanh(.5 * x))
-        # return 1/(1+np.exp(-x))
-
     # gradAscent
-    def fit(self, X_train, y_train, step_size=0.01, max_iter=1000, tol=1e-3):
-        X = np.mat(X_train.copy())  # convert to NumPy matrix
-        y = np.mat(y_train.copy()).transpose()  # convert to NumPy matrix
+    def fit(self, X_train, y_train, step_size=0.01, max_iter=10000, tol=1e-4):
+        X = np.matrix(X_train.copy())  # convert to NumPy matrix
+        y = np.matrix(y_train.copy()).transpose()  # convert to NumPy matrix
 
         # label -1 by to 0 if exists
         y[y == -1] = 0
@@ -24,53 +43,27 @@ class GD_gv():
         m, n = np.shape(X)
 
         # add bias term $b$
-        #X = np.column_stack((X, np.ones((m, 1))))
+        X = np.column_stack((X, np.ones((m, 1))))
 
         # initial for nesterov accelerated gradient descent
-
-        theta_prev = 0
-        theta_curr = 1
-        gamma = 1
-        w = np.zeros((n, 1))
-        b = 0
-        w_prev = w
-        b_prev = b
+        l = 1
+        w = np.ones((n + 1, 1))        
         for k in range(max_iter):  # heavy on matrix operations
-
+            w_prev = w
             # compute loss and its gradient
-            h = self.sigmoid(X * w + b)  # matrix mult
-            error = y - h  # vector subtraction\
-            gradient = - X.T * error 
-            gradient_b = - np.ones((1,m)) * error
+            w, l = backtracking(l, w, X, y)
 
-            # update w
-            w_curr = w - step_size * gradient
-            b_curr = b - step_size * gradient_b
-            w = (1 - gamma) * w_curr + gamma * w_prev
-            w_prev = w_curr
-
-            b = (1 - gamma) * b_curr + gamma * b_prev
-            b_prev = b_curr
-
-            theta_tmp = theta_curr
-            theta_curr = (1 + np.sqrt(1 + 4 * theta_prev * theta_prev)) / 2
-            theta_prev = theta_tmp
-
-            gamma = (1 - theta_prev) / theta_curr
-
-            # stop criterion
-            # if np.linalg.norm(error) < 1e-3:
-            # break
             # use the norm of gradient
-            if np.linalg.norm(gradient) < tol:
+            if np.linalg.norm(w-w_prev) < tol:
                 break
                 
         if k == max_iter - 1:
             print('convergence fail, the current norm of gradient is {}'.format(
-                np.linalg.norm(gradient)))
+                np.linalg.norm(w-w_prev)))
 
         w = np.array(w).flatten()
-        b = b[0,0]
+        w = w[0:w.shape[0]-1]
+        b = w[-1]
         #b = w[n]
         #w = w[0:n]
         clf = Clf(w, b)
