dfad9a25b614d676bb0a2fc1cdf21f6f4bc1d711
Zenan <lizenan2018@gmail.com>
2020-10-06 01:46:27 +0800

    APG_L1_gv (could not solve ill-condition problem)

--- algorithms/Svm/APG/L1/APG_L1_gv.py
+++ algorithms/Svm/APG/L1/APG_L1_gv.py
@@ -11,16 +11,18 @@ from cvxopt import matrix,solvers
 def backtracking(l0, x0, p, q, low, up):
     # update x
     beta = 0.5
-    l = l0
+    l = 1e-3
     L0 = 0.5*x0.T*(p*x0) + q.T*x0
+    L0 = L0.item()
     g0 = p*x0 + q    
-    for k in range(128):
+    for k in range(8):
         xp = x0 - l * g0
         xp[xp < low] = low
         xp[xp > up] = up  
         Lx = 0.5*xp.T*(p*xp) + q.T*xp
+        Lx = Lx.item()
         gt = (x0-xp) / l
-        if Lx > L0 - l *(g0.T*gt) + 0.5*l*gt.T*(gt):
+        if Lx > L0 - l *(g0.T*gt).item() + 0.5*l*(gt.T*(gt)).item():
             l = beta * l
         else:
             break
@@ -28,83 +30,116 @@ def backtracking(l0, x0, p, q, low, up):
     return xp, l
 
 
-def projected_apg(p, q, bounds, step_size=0.1, max_iter=5000):
-    m = p.shape[0]
+def projected_apg(X, y, max_iter=10000):    
+    m, n = X.shape    
+    X = np.column_stack((X, np.ones((m, 1)))) # if cvx has been used for verify, `#` this line
+    y = y.astype(np.float64)
+    data_num = len(y)
+    C = 1.0
+    kernel = np.dot(X, np.transpose(X))
+    p = np.matrix(np.multiply(kernel, np.outer(y, y)), np.float64)
+    q = np.matrix(-np.ones([data_num, 1], np.float64))  
+    bounds = (0, C)  
     low, up = bounds    
 
-    x = np.ones((m, 1), np.float64) * 0.5
-    y = x
-
-    p = p + np.diag(np.ones(m, np.float64)) * np.mean(p) 
-    v, w = np.linalg.eigh(p)   
-
-    l = 1/v[-1] - 1e-10
+    x = np.ones((m, 1), np.float64) * 0.1
+    
+    # u, s, v = np.linalg.svd(p)
+    # print(np.diag(s))
+    l = 1
+    t = 1
 
     for k in range(max_iter):  # heavy on matrix operations
 
-        # p = p + np.eye(p.shape[0]) * (.1/(k+1))
         # saving previous x
-        y = x
+        x_prev = x; t_prev = t
+        x_curr, l = backtracking(l, x, p, q, low, up)
+        # t = (1+np.sqrt(1+4*t_prev**2)) / 2
+        ## acc
+        # x = x_curr + (t_prev - 1)/(t) * (x_curr - x_prev)
+        x = x_curr
 
-        x, l = backtracking(l, y, p, q, low, up)
+        dual = -(0.5*x.T*(p*x) + q.T*x)
+        dual = dual.item()
+        y1 = np.reshape(y, (-1, 1))
+        lambda1 = np.multiply(x, y1)
+        w = np.dot(X.T, lambda1)
+        w = np.matrix(w).reshape(-1, 1)      
+        tmp = np.maximum(1-np.multiply(y1, X*w),0)
+        primal = 0.5*np.linalg.norm(w)**2 + 1 * np.sum(tmp)
+        primal = primal.item()
 
         # stop criteria            
-        rnormw = np.linalg.norm(y-x) / (1+np.linalg.norm(x))  
-        if  k > 1 and rnormw < 1e-6:
+        if np.abs(dual-primal)/(1+np.abs(dual)+np.abs(primal)) < 1e-6:
             break
-    return x
+    # y1 = np.reshape(y, (-1, 1))
+    # alpha1 = alpha_svs
+    # print('sup', np.sum(alpha1 > 1e-5))
+    # lambda1 = np.multiply(alpha1, y1)
+    print(primal, dual)
+    w = np.dot(X.T, lambda1)
+    w = np.array(w).reshape(-1) 
+    w = w[0:w.shape[0]-1] 
+    b = w[-1]
+    
+    
+    return w, b
 
 
 
 class APG_L1_gv():
-    def fit(self, X, y):
-
+    def fit(self, X, y, w=None):
+        y[y == 0] = -1
         # add logitR to verify the correctness
         from sklearn.svm import LinearSVC
-        SVM = LinearSVC(loss='hinge', tol=1e-5, verbose=1, max_iter=1000).fit(X, np.array(y).ravel())
+        SVM = LinearSVC(loss='hinge', tol=1e-6, max_iter=100000, verbose=1).fit(X, np.array(y).ravel())
         w1 = SVM.coef_; b1 = SVM.intercept_
         w1 = w1.reshape(-1); b1 = b1[0]        
-
-        m, n = X.shape
-        X = np.column_stack((X, np.ones((m, 1))))
-        y = y.astype(np.float64)
-        data_num = len(y)
-        C = 1.0
-        kernel = np.dot(X, np.transpose(X))
-        p = np.matrix(np.multiply(kernel, np.outer(y, y)), np.float64)   
-        q = np.matrix(-np.ones([data_num, 1], np.float64))
-        # p = p * np.linalg.norm(p)
-        # q = q * np.linalg.norm(p)
-
-        bounds = (0, C)
         
-        p = matrix(p); q = matrix(q);
-        g_1 = -np.eye(data_num)
-        h_1 = np.zeros([data_num, 1], np.float64)
+        #### solve by solver.qp
+        # m, n = X.shape
+        # X = np.column_stack((X, np.ones((m, 1))))
+        # y = y.astype(np.float64)
+        # data_num = len(y)
+        # C = 1.0
+        # kernel = np.dot(X, np.transpose(X))
+        # p = np.matrix(np.multiply(kernel, np.outer(y, y)), np.float64)    
+        # q = np.matrix(-np.ones([data_num, 1], np.float64))
+        # p = p / np.linalg.norm(q); q = q / np.linalg.norm(q);
+        # p = matrix(p); q = matrix(q);
+        # g_1 = -np.eye(data_num)
+        # h_1 = np.zeros([data_num, 1], np.float64)
 
-        g_2 = np.eye(data_num)
-        h_2 = np.zeros([data_num, 1], np.float64) + C
+        # g_2 = np.eye(data_num)
+        # h_2 = np.zeros([data_num, 1], np.float64) + C
+    
+        # g = matrix(np.vstack((g_1, g_2)))
+        # h = matrix(np.vstack((h_1, h_2)))
+        # solvers.options['show_progress'] = False
+        # solvers.options['abstol'] = 1e-10
+        # solvers.options['restol'] = 1e-10
+        # solvers.options['featol'] = 1e-10
+        # solvers.options['maxiters'] = 1000
+        # sol = solvers.qp(p, q, g, h)
+        # alpha_svs = np.array(sol['x']) 
+        # x = np.mat(alpha_svs)
+        # print(np.sum(alpha_svs>1e-5)) 
 
-        g = matrix(np.vstack((g_1, g_2)))
-        h = matrix(np.vstack((h_1, h_2)))
+        # dual = -(0.5*x.T*(p*x) + q.T*x)
+        # dual = dual.item()
+        # y1 = np.reshape(y, (-1, 1))
+        # lambda1 = np.multiply(x, y1)
+        # w = np.dot(X.T, lambda1)
+        # w = np.matrix(w).reshape(-1, 1)        
+        # tmp = np.maximum(1-np.multiply(y1, X*w),0)
+        # primal = 0.5*np.linalg.norm(w)**2 + 1 * np.sum(tmp)
+        # primal = primal.item()
+        # print('cvx:', dual, primal) 
+        # w = np.array(w).reshape(-1)
+        # w = w[0:w.shape[0]-1] 
+        # b = w[-1]    
 
-        # a = matrix(y, (1, data_num))
-        # b = matrix(0.)
-        solvers.options['show_progress'] = True
-        # sol = solvers.qp(p, q, g, h, a, b)
-        sol = solvers.qp(p, q, g, h)
-        alpha_svs = np.array(sol['x'])  
-
-        # alpha_svs = projected_apg(p, q, bounds)   
-
-        # alpha_svs = alpha_svs1
-        y1 = np.reshape(y, (-1, 1))
-        alpha1 = alpha_svs
-        lambda1 = np.multiply(y1,alpha1)  
-        w = np.dot(X.T, lambda1)
-        w = np.array(w).reshape(-1)
-        b = w[-1]
-        w = w[0:w.shape[0]-1]
+        w, b = projected_apg(X, y)
 
         print('diff', np.linalg.norm(w1-w), b, b1)
          
