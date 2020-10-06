775e5f5286201367ec90d2b9a1d243859603950f
Zenan <lizenan2018@gmail.com>
2020-10-06 13:58:49 +0800

    APG_L1_gv(verified successfully)

--- algorithms/Svm/APG/L1/APG_L1_gv.py
+++ algorithms/Svm/APG/L1/APG_L1_gv.py
@@ -8,79 +8,87 @@ from cvxopt import matrix,solvers
 
 ## accelerate proximal gradient method
 
-def backtracking(l0, w0, X, y):
+def backtracking(l0, x0, p, q, low, up):
     # update x
-    m, n = X.shape
-    beta = 0.5; alpha = 0.01
-    l = 1e-3
-    w = np.matrix(w0).reshape(-1, 1)   
-    y1 = np.reshape(y, (-1, 1))   
-    tmp = np.maximum(1-np.multiply(y1, X*w),0)
-    L0 = 0.5*np.linalg.norm(w)**2 + 1 * np.sum(tmp)
-    tmp = tmp != 0
-    tmp = np.multiply(y1, tmp)
-    g0 = -X.T * tmp + w0
-    if np.linalg.norm(g0) < 1e-4:
-        wp = w0; l = l0
-        print('grad', np.linalg.norm(g0))
-        return wp, l
-
-    for k in range(32):
-        wp = w0 - l * g0
-        w = np.matrix(wp).reshape(-1, 1)   
-        y1 = np.reshape(y, (-1, 1))   
-        tmp = np.maximum(1-np.multiply(y1, X*w),0)
-        Lw = 0.5*np.linalg.norm(w)**2 + 1 * np.sum(tmp)
-        if Lw < L0 - l * alpha * (g0.T*g0):
-            break
-        else:
+    beta = 0.5
+    l = 1e-1
+    L0 = 0.5*x0.T*(p*x0) + q.T*x0
+    L0 = L0.item()
+    g0 = p*x0 + q    
+    for k in range(8):
+        xp = x0 - l * g0
+        xp[xp < low] = low
+        xp[xp > up] = up  
+        Lx = 0.5*xp.T*(p*xp) + q.T*xp
+        Lx = Lx.item()
+        gt = (x0-xp) / l
+        if Lx > L0 - l *(g0.T*gt).item() + 0.5*l*(gt.T*(gt)).item():
             l = beta * l
+        else:
+            break
+            
+    return xp, l
 
-    return wp, l
 
-
-def projected_apg(X, y, max_iter=30000):    
-    C = 1.0 
-    m, n = X.shape
+def projected_apg(X, y, max_iter=5000):    
+    m, n = X.shape    
     X = np.column_stack((X, np.ones((m, 1)))) # if cvx has been used for verify, `#` this line
-    x = np.ones((n+1, 1), np.float64) * 0.5
+    y = y.astype(np.float64)
+    data_num = len(y)
+    C = 1.0
+    kernel = np.dot(X, np.transpose(X))
+    p = np.matrix(np.multiply(kernel, np.outer(y, y)), np.float64)
+    q = np.matrix(-np.ones([data_num, 1], np.float64))  
+    bounds = (0, C)  
+    low, up = bounds    
+
+    x = np.zeros((m, 1), np.float64)
+    
+    # v, u = np.linalg.eigh(p)
+    # l = 1/v[-1] - 1e-10
     l = 1
     t = 1
 
     for k in range(max_iter):  # heavy on matrix operations
 
         # saving previous x
-        x_prev = x; t_prev = t;
-        x_curr, l = backtracking(l, x, X, y)
-        # stop criteria            
-        if np.linalg.norm(x-x_curr) == 0:
-            print('converge')
-            break        
+        x_prev = x; t_prev = t
+        x_curr, l = backtracking(l, x, p, q, low, up)
+        # t = (1+np.sqrt(1+4*t_prev**2)) / 2
+        ## acc
+        # x = x_curr + (t_prev - 1)/(t) * (x_curr - x_prev)
         x = x_curr
 
-        w = np.matrix(x).reshape(-1, 1)   
+        dual = -(0.5*x.T*(p*x) + q.T*x)
+        dual = dual.item()
         y1 = np.reshape(y, (-1, 1))
+        lambda1 = np.multiply(x, y1)
+        w = np.dot(X.T, lambda1)
+        w = np.matrix(w).reshape(-1, 1)      
         tmp = np.maximum(1-np.multiply(y1, X*w),0)
         primal = 0.5*np.linalg.norm(w)**2 + 1 * np.sum(tmp)
-        primal = primal.item()        
-        # if k % 100 == 0:
-            # print(primal)
+        primal = primal.item()
 
-    # y1 = np.reshape(y, (-1, 1))
-    # alpha1 = alpha_svs
-    # print('sup', np.sum(alpha1 > 1e-5))
-    # lambda1 = np.multiply(alpha1, y1)
-    w = np.array(x).reshape(-1) 
-    w = w[0:w.shape[0]-1] 
+        # tmp = tmp != 0
+        # tmp = np.multiply(y1, tmp)
+        # tmp = np.tile(tmp, (1, n+1))
+        # g0 = -np.sum(np.multiply(X, tmp), axis=0).T + w        
+
+
+        # stop criteria            
+        if np.abs(dual-primal)/(1+np.abs(dual)+np.abs(primal)) < 1e-12:
+            break
+
+    w = np.dot(X.T, lambda1)
+    w = np.array(w).reshape(-1) 
     b = w[-1]
-    
-    
+    w = w[0:w.shape[0]-1] 
     return w, b
 
 
 
 class APG_L1_gv():
-    def fit(self, X, y):
+    def fit(self, X, y, w=None):
         y[y == 0] = -1
         # add logitR to verify the correctness
         from sklearn.svm import LinearSVC
