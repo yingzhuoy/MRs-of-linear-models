145e09b4b0e524f422a81310289f53e95597219d
Zenan <lizenan2018@gmail.com>
2020-10-23 15:56:23 +0800

    temp SQP_L1_gv

--- algorithms/Svm/IPM/L1/IPM_L1_gv.py
+++ algorithms/Svm/IPM/L1/IPM_L1_gv.py
@@ -1,139 +1,134 @@
 import numpy as np
 from numpy import linalg
 from algorithms.clf import Clf
-
 """
 Preconditioned Conjugate Gradient Method
 """
 
-def precond(M, r):
-    q = M * r
-    return q
 
+def inner_point(X, y, max_iter=1000):
+    m, n = X.shape
+    X = np.column_stack((X, np.ones((m, 1))))
+    y = y.astype(np.float64)
+    data_num = len(y)
+    C = 1.0
+    kernel = np.dot(X, np.transpose(X))
+    # + np.diag(np.ones(data_num, np.float64)) * .5/C
+    p = np.matrix(np.multiply(kernel, np.outer(y, y)))
+    q = np.matrix(-np.ones([data_num, 1], np.float64))
 
-def cg(A, b, x=None, tol=1.0e-3, max_iter=50):
-    # precondition  
-    A = np.matrix(A)
-    b = np.matrix(b)    
-    normb = np.linalg.norm(b, 'fro')
-    m = b.shape[0]
-    if np.linalg.norm(A,'fro') > 1e-12:
-        M = np.linalg.inv(np.diag(np.diag(A.T*A)))
-    else:
-        M = np.eye(m)
-    
-    x = np.zeros((m, 1))
-    Aq = np.dot(A, x)    
-    r = b - Aq
-    q = precond(M, r)       
-    tau_old = np.linalg.norm(q)
-    rho_old = np.dot(r.T, q)
-    theta_old = 0
-    Ad = np.zeros((m, 1))
-    d = np.zeros((m, 1))
-    res = r
-    
-    tiny = 1e-30
-    for i in range(max_iter):
-        Aq = np.dot(A, q)
-        sigma = np.dot(q.T, Aq)
-        
-        if abs(sigma.item()) < tiny:
-            break
-        else:
-            alpha = rho_old / sigma;
-            alpha = alpha.item()
-            r = r - alpha * Aq
-        u = precond(M, r)
+    bounds = (0, C)
 
-        theta = np.linalg.norm(u)/tau_old
-        c = 1 / np.sqrt(1+theta*theta)
-        tau = tau_old * theta * c
-        gam = c*c*theta_old*theta_old
-        eta = c*c*alpha
-        d = gam * d + eta * q
-        x = x + d
+    low, up = bounds
+    x = np.zeros([m, 1])
 
-        # stop
-        Ad = gam*Ad+eta*Aq
-        res = res - Ad
-        if np.linalg.norm(res) < tol*normb:
-            break
-        else:
-            rho = np.dot(r.T, u)
-            beta = rho / rho_old
-            beta = beta.item()
-            q = u + beta * q
-
-        rho_old = rho
-        tau_old = tau
-        theta_old = theta
-    return x
-
-
-def inner_point(p, q, bounds, step_size=0.1, max_iter=50):
-    m = p.shape[0]
-    low, up = bounds    
-    x = np.ones((m,1)) * ((low+up)/2)
-    p = p + 1e-3*np.diag(np.ones(m, np.float64))
-    t = 1
-    mu = 10
     for k in range(max_iter):  # heavy on matrix operations
+        for i in range(m):
+            # remove optimal alpha
+            g = p*x+q
+            if low < x[i] and x[i] < up:
+                if g[i] == 0:
+                    continue
+
+            if x[i] == low:
+                if np.minimum(0, g[i]) == 0:
+                    continue
+
+            if x[i] == up:
+                if np.maximum(0, g[i]) == 0:
+                    continue
+
+
+            tmpx = x.copy()
+            tmpx[i, 0] = 0
+            temp = (p[i, :] * tmpx) + q[i]
+            # if temp > 0 and x[i] == 0:
+                # continue
+            temp = temp.item()
+            if p[i, i] > 0:
+                xi = -(temp / p[i, i]).item()
+                xi = np.maximum(low, xi)
+                xi = np.minimum(up, xi)
+            elif p[i, i] < 0:
+                print('error')
+            else:
+                if temp > 0:
+                    xi = low
+                else:
+                    xi = up
+            x[i, 0] = xi
+
+
+        # for u in range(m):
+        #     i = -1-u
+
+        #     tmpx = x.copy()
+        #     tmpx[i, 0] = 0
+        #     temp = (p[i, :] * tmpx) + q[i]
+        #     # if temp > 0 and x[i] == 0:
+        #         # continue
+        #     temp = temp.item()
+        #     if p[i, i] > 0:
+        #         xi = -(temp / p[i, i]).item()
+        #         xi = np.maximum(low, xi)
+        #         xi = np.minimum(up, xi)
+        #     elif p[i, i] < 0:
+        #         print('error')
+        #     else:
+        #         if temp > 0:
+        #             xi = low
+        #         else:
+        #             xi = up
+        #     x[i, 0] = xi
+
+
         
-        # saving previous x
-        y = x
 
-        # compute loss and its gradient
-        gradient = p*x + q
-        hessian = p
-        
-        #
-        if low != -np.inf:
-            gradient = gradient + (1/(low-x)) * (1/t)
-            hessian = hessian + (np.diag(1/(np.square(low-x)))) * (1/t)
+        dual = -(0.5 * x.T * (p * x) + q.T * x)
+        dual = dual.item()
+        y1 = np.reshape(y, (-1, 1))
+        lambda1 = np.multiply(x, y1)
+        w = np.dot(X.T, lambda1)
+        w = np.matrix(w).reshape(-1, 1)
+        tmp = np.maximum(1 - np.multiply(y1, X * w), 0)
+        primal = 0.5 * np.linalg.norm(w)**2 + 1 * np.sum(tmp)
+        primal = primal.item()
 
-        if up != np.inf:
-            gradient = gradient + (1/(x-up)) * (1/t)
-            hessian = hessian + (np.diag(1/(np.square(x-up)))) * (1/t)
-
-        d = cg(hessian, gradient)
-
-        x = x-d
-
-        t = mu * t
-
-        # stop criteria            
-        if  k > 1 and np.linalg.norm(gradient) < 1e-6:
-            #print('convergence!')
+        # stop criteria
+        if k % 10 == 0:
+            print(np.abs(dual - primal) / (1 + np.abs(dual) + np.abs(primal)))
+        # print(np.abs(dual - primal) / (1 + np.abs(dual) + np.abs(primal)))
+        if np.abs(dual - primal) / (1 + np.abs(dual) + np.abs(primal)) < 1e-12:
+            print('success')
             break
 
-    return y
+    return w
 
 
-
-#L1-svm
+# L1-svm
 class IPM_L1_gv():
+
     def fit(self, X, y):
+        y[y == 0] = -1
+        # add logitR to verify the correctness
+        from sklearn.svm import LinearSVC
+        SVM = LinearSVC(loss='hinge', tol=1e-6, max_iter=100000, verbose=1).fit(X, np.array(y).ravel())
+        w1 = SVM.coef_; b1 = SVM.intercept_
+        w1 = w1.reshape(-1); b1 = b1[0] 
+        #       
         m, n = X.shape
-        X = np.column_stack((X, np.ones((m, 1))))
-        y = y.astype(np.float64)
-        data_num = len(y)
-        C = 1.0
-        kernel = np.dot(X, np.transpose(X))
-        p = np.matrix(np.multiply(kernel,np.outer(y, y))) # + np.diag(np.ones(data_num, np.float64)) * .5/C
-        q = np.matrix(-np.ones([data_num, 1], np.float64))
-
-        bounds = (0, C)
-        alpha_svs = inner_point(p, q, bounds)        
-
-        y1 = np.reshape(y, (-1, 1))
-        alpha1 = alpha_svs
-        lambda1 = np.multiply(y1,alpha1)   
-        w = np.dot(X.T, lambda1)
+        import time
+        t1 = time.time()
+        w = inner_point(X, y)
+        t2 = time.time()
+        print(t2-t1, 's')
         w = np.array(w).reshape(-1)
+
         # b = np.mean(y1-np.reshape(np.dot(w, np.transpose(X)), [-1, 1]))
         b = w[n]
         w = w[0:n]
+
+        print('diff', np.linalg.norm(w1-w), b, b1)
 
         clf = Clf(w, b)
         return clf
